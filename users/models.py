dd09908a6255105c0b5b32a84004c29e5bf3a582
6igsm0ke <kosbayevadilet@gmail.com>
2025-07-28 18:28:37 +0500

    fix updated_at changed from auto_now_add to auto_now

--- users/models.py
+++ users/models.py
@@ -15,7 +15,7 @@ class DefaultTypeClass(models.Model):
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
